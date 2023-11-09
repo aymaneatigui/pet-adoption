63c02548c0b081bedbbd73d03d203aa89a906e6e
aymane <atigui.cs@gmail.com>
2023-11-09 16:06:45 +0100

    change http to https

--- src/Fetch/fetchPet.jsx
+++ src/Fetch/fetchPet.jsx
@@ -1,7 +1,7 @@
 const fetchPet = async ({ queryKey }) => {
   const id = queryKey[1]
   console.log(id)
-  const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`)
+  const res = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`)
 
   if(!res.ok){
     throw new Error(`details/${id} fetch not ok`)
--- src/Fetch/fetchSearch.jsx
+++ src/Fetch/fetchSearch.jsx
@@ -2,7 +2,7 @@ const fetchSearch = async ({ queryKey }) => {
   const { animal, breed, location } = queryKey[1];
 
   const res = await fetch(
-    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
+    `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
   );
 
   if(!res.ok){
