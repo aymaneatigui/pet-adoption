8440e75535a7805e8521f51cc325d26a1bc9b243
aymane <atigui.cs@gmail.com>
2023-11-15 22:19:03 +0100

    add lazy and suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import AdoptePetContext from "./AdoptePetContext.jsx";
 // import SearchParams from "./SearchParams.jsx";
-// import Details from "./Details.jsx";
+// import Details from "./Details.jsx
+
+const Details = lazy(() => import("./Details.jsx"));
+const SearchParams = lazy(() => import("./SearchParams.jsx"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,15 +26,23 @@ const App = () => {
       <Router>
         <QueryClientProvider client={queryClient}>
           <AdoptePetContext.Provider value={adoptePets}>
-            <header>
-              <Link to={"/"}>
-                <h1>Adopte me!</h1>
-              </Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h1 className="loader">🌀</h1>
+                </div>
+              }
+            >
+              <header>
+                <Link to={"/"}>
+                  <h1>Adopte me!</h1>
+                </Link>
+              </header>
+              <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+            </Suspense>
             <ReactQueryDevtools />
           </AdoptePetContext.Provider>
         </QueryClientProvider>
